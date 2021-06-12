708d0ff2b6df03240172301d0ea8ed1a05c4c36a
B Febles <61816949+bfebles@users.noreply.github.com>
2021-06-12 17:20:18 -0500

    Update covid_snpsift.sh

--- covid_snpsift.sh
+++ covid_snpsift.sh
@@ -12,6 +12,6 @@ for i in *.snpEFF.ann.vcf; do
 
      cat ${F}.snpsift.vcf | java -jar /home/binita/snpEff/SnpSift.jar filter "(QUAL >= 5000)" > ${F}.snpsift.filtered.vcf;
 
-     java -jar /home/binita/snpEff/SnpSift.jar extractFields ${F}.snpsift.filtered.vcf CHROM POS REF ALT VARTYPE QUAL FILTER DP AF SB "ANN[0].GENE" "EFF[0].EFFECT" "EFF[0].IMPACT" "EFF[0].BIOTYPE" > ${F}.snpsi$
+     java -jar /home/binita/snpEff/SnpSift.jar extractFields ${F}.snpsift.filtered.vcf CHROM POS REF ALT VARTYPE QUAL FILTER DP AF SB "ANN[0].GENE" "EFF[0].EFFECT" "EFF[0].IMPACT" "EFF[0].BIOTYPE" > ${F}.snpsift.tsv;
 
 done
