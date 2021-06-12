8928a1f301e8b95c056e643cb76624fb1ecab136
B Febles <61816949+bfebles@users.noreply.github.com>
2021-06-12 17:16:33 -0500

    Create covid_snpsift.sh

---
+++ covid_snpsift.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Add variant type field
+#Filter quality threshold
+
+for i in *.snpEFF.ann.vcf; do
+
+     F=${i%.snpEFF.ann.vcf};
+
+     echo "Running:$F";
+
+     java -jar /home/binita/snpEff/SnpSift.jar varType $i > ${F}.snpsift.vcf;
+
+     cat ${F}.snpsift.vcf | java -jar /home/binita/snpEff/SnpSift.jar filter "(QUAL >= 5000)" > ${F}.snpsift.filtered.vcf;
+
+     java -jar /home/binita/snpEff/SnpSift.jar extractFields ${F}.snpsift.filtered.vcf CHROM POS REF ALT VARTYPE QUAL FILTER DP AF SB "ANN[0].GENE" "EFF[0].EFFECT" "EFF[0].IMPACT" "EFF[0].BIOTYPE" > ${F}.snpsi$
+
+done
