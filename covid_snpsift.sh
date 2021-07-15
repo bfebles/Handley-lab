44d90b3d4e33aa0442c44739e9357dbf7624eeea
B Febles <61816949+bfebles@users.noreply.github.com>
2021-07-15 11:25:10 -0500

    Update covid_snpsift.sh

--- covid_snpsift.sh
+++ covid_snpsift.sh
@@ -8,10 +8,10 @@ for i in *.snpEFF.ann.vcf; do
 
      echo "Running:$F";
 
-     java -jar /home/binita/snpEff/SnpSift.jar varType $i > ${F}.snpsift.vcf;
+     java -jar /opt/apps/snpEff/SnpSift.jar varType $i > ${F}.snpsift.vcf;
 
-     cat ${F}.snpsift.vcf | java -jar /home/binita/snpEff/SnpSift.jar filter "(QUAL >= 5000)" > ${F}.snpsift.filtered.vcf;
+     cat ${F}.snpsift.vcf | java -jar /opt/apps/snpEff/SnpSift.jar filter "(QUAL >= 5000)" > ${F}.snpsift.filtered.vcf;
 
-     java -jar /home/binita/snpEff/SnpSift.jar extractFields ${F}.snpsift.filtered.vcf CHROM POS REF ALT VARTYPE QUAL FILTER DP AF SB "ANN[0].GENE" "EFF[0].EFFECT" "EFF[0].IMPACT" "EFF[0].BIOTYPE" > ${F}.snpsift.tsv;
+     java -jar /opt/apps/snpEff/SnpSift.jar extractFields ${F}.snpsift.filtered.vcf CHROM POS REF ALT VARTYPE QUAL FILTER DP AF SB "ANN[0].GENE" "EFF[0].EFFECT" "EFF[0].IMPACT" "EFF[0].BIOTYPE" > ${F}.snpsift.tsv;
 
 done
