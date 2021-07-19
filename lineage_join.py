15c92cf080f033814ca6c812e9aaae07cd83ee70
B Febles <61816949+bfebles@users.noreply.github.com>
2021-07-19 11:27:13 -0500

    Update lineage_join.py

--- lineage_join.py
+++ lineage_join.py
@@ -11,7 +11,7 @@ outdir = str(path.parent)
 pangolin = pd.read_csv('lineage_report.csv')
 nextclade = pd.read_csv('nextclade_results.tsv', sep = '\t')
 pangolin.rename(columns={'taxon': 'seqName','lineage':'Pangolin_lineage'},inplace=True)
-final_lineage = pd.merge(left=pangolin,right=nextclade,left_on='seqName',right_on='seqName')
+final_lineage = pd.merge(left=pangolin,right=nextclade, how='left',left_on='seqName',right_on='seqName')
 final_lineage.insert(2, 'clade', final_lineage.pop("clade"))
 final_lineage.to_csv('final_lineage.csv',index=False)
     
